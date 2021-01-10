010dfcf81c0af5ef49ce3f6811d18f41b7f83d1c
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2021-01-10 22:17:50 +0900

    シーンクラスを作成、テストコードを追加

--- sas/sas_scene.py
+++ sas/sas_scene.py
@@ -3,8 +3,16 @@
 
 
 class Scene():
-    """シーンクラス."""
+    """シーンクラス(数値で管理する)."""
 
     def __init__(self):
         """コンストラクタ."""
-        self.__scene = 0
+        self.__number = 0
+
+    def set(self, num: int):
+        """数値を設定."""
+        self.__number = num
+
+    def get(self) -> int:
+        """数値を取得."""
+        return self.__number
